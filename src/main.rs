04db6d0b5a70eb4dbe9a1f80283fc8d84a272883
charmstr <charmstr@MacBook-Pro-de-charmstr.local>
2022-04-25 16:16:57 +0200

    binary accepting one argument OK

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
-    println("! coucou tony");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+       panic!("usage: ./computer_v1 \"1 + X^2\"");
+    }
+    
+    let input = args[1].clone();
+    println!("{}, {}", input, args[1]);
 }
